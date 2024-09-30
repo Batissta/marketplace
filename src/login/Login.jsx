58699a8cdc737e00e7aa2b464aeee3d19cbbaa27
Talles-Oliveira <132765451+talles1x@users.noreply.github.com>
2024-09-30 18:15:07 -0300

    feat - login
    foram feitas alterações na login page, para visar uma página intuitiva e simples.

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -6,10 +6,6 @@ import { Route, Routes } from "react-router-dom";
 const Login = () => {
   return (
     <section className={styles.loginForm}>
-      <img
-        src="https://www.vendaecia.com.br/wp-content/uploads/2021/07/produtos-para-dropshipping.jpg"
-        alt="imagem ilustrando um marketplace na visão do desenvolvedor."
-      />
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/cadastro" element={<LoginCadastro />} />
